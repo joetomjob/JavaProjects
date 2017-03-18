8e0f5ede80b2c284ae34532408e1efd78eb830fe
Joe Tom Job <joetomjob@gmail.com>
2017-03-18 17:29:15 -0400

    Update MorseVowel.java

--- src/AlgorithmsHW/HW3/MorseVowel.java
+++ src/AlgorithmsHW/HW3/MorseVowel.java
@@ -1,9 +1,10 @@
 package AlgorithmsHW.HW3;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 /**
- * Created by Admin on 3/15/2017.
+ * Created by Joe Tom Job.
  */
 public class MorseVowel {
     public static void main(String[] args) {
@@ -15,25 +16,68 @@ public class MorseVowel {
         //take the values of n characters representing the morse code
         String code = in.nextLine();
 
-        char[] codeArray = new char[n];
-        codeArray = code.toCharArray();
+        String[][] table = new String[5][2];
+        table[0][0] = "E";
+        table[0][1] = ".";
 
-        String[][] vowelList = new String[5][2];
-        vowelList[0][0] = "E";
-        vowelList[0][1] = ".";
+        table[1][0] = "I";
+        table[1][1] = "..";
 
-        vowelList[1][0] = "I";
-        vowelList[1][1] = "..";
+        table[2][0] = "A";
+        table[2][1] = ".-";
 
-        vowelList[2][0] = "A";
-        vowelList[2][1] = ".-";
+        table[3][0] = "O";
+        table[3][1] = "---";
 
-        vowelList[3][0] = "O";
-        vowelList[3][1] = "---";
-
-        vowelList[4][0] = "U";
-        vowelList[4][1] = "..-";
+        table[4][0] = "U";
+        table[4][1] = "..-";
 
 
+        int[] list = new int[code.length() + 1];
+        list[0] = 0;
+
+        for (int i = 1; i < code.length()+1; i++) {
+
+            if (i > 0) {
+                int k = count(table, Character.toString(code.charAt(i - 1)));
+                if (k > 0) {
+                    if(list[i-1]==0 && i-1 ==0)
+                        list[i] += k;
+                    else{
+                        list[i] += list[i-1];}
+                }
+                if (i > 1) {
+                    int l = count(table, "" + code.charAt(i - 2)+ code.charAt(i - 1));
+                    if (l > 0) {
+                        if(list[i-2]==0 && i-2 ==0)
+                            list[i] += l;
+                        else
+                            list[i] += list[i - 2];
+                    }
+                }
+                if (i > 2) {
+                    int m = count(table, "" + code.charAt(i - 3)+ code.charAt(i - 2) + code.charAt(i - 1));
+                    if (m > 0) {
+                        if(list[i - 3]==0 && i-3==0)
+                            list[i] += m;
+                        else
+                            list[i] += list[i - 3];
+                    }
+                }
+
+            }
+        }
+        System.out.println(list[code.length()]);
+
+    }
+
+    private static int count(String[][] table, String s) {
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            if (table[i][1].equals(s)) {
+                count++;
+            }
+        }
+        return count;
     }
 }
